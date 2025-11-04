f78b6000928f975c3772ee784cb6503dff9e0286
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-11-04 19:03:30 +0200

    feat(ui): add splash screen with animation

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bookly/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      home: const SplashView(),
+    );
+  }
+}
