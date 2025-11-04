f19f05ff71fbb36a995ce6369c7ba4f6d72853d9
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-11-04 21:15:12 +0200

    feat(ui): add custom appbar to home

---
+++ lib/features/home/presentation/views/widgets/custom_appbar.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/core/utlis/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(AssetsData.logo, width: 40, height: 40),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search, size: 38)),
+        ],
+      ),
+    );
+  }
+}
