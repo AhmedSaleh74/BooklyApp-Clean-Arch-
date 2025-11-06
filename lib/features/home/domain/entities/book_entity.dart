7566a75be44434891cdcc66b068099316388ee0a
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-11-06 13:30:35 +0200

    feat(home): added Book entity for domain layer

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String bookImage;
+  final String bookTitle;
+  final String bookAuthor;
+  final num bookRating;
+  final num bookPrice;
+
+  BookEntity({
+    required this.bookImage,
+    required this.bookTitle,
+    required this.bookAuthor,
+    required this.bookRating,
+    required this.bookPrice,
+  });
+}
