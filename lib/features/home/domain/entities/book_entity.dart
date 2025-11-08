991e908bb9c9bc7bdceac8c09bd136350b3ab7e6
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-11-08 14:42:24 +0200

    refactor(home): update book model structure

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,11 +1,13 @@
 class BookEntity {
-  final String bookImage;
+  final String bookID;
+  final String? bookImage;
   final String bookTitle;
-  final String bookAuthor;
-  final num bookRating;
-  final num bookPrice;
+  final String? bookAuthor;
+  final num? bookRating;
+  final num? bookPrice;
 
   BookEntity({
+    required this.bookID,
     required this.bookImage,
     required this.bookTitle,
     required this.bookAuthor,
