ed9c4a0c26aceeae155c49febbaec648c9b3a8a7
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-11-11 20:22:30 +0200

    feat(di): integrate GetIt for dependency injection
    - added GetIt package to manage app dependencies
    - setup service locator pattern for easy access to repositories and view models
    - registered all core services and feature-specific dependencies
    - updated project structure to follow DI best practices

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/features/home/data/repositories/home_repo_impl.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/use_cases/get_featured_books_use_case.dart';
+import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
+
+import 'core/utils/functions/setup_service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // تهيئة Hive للعمل مع Flutter
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
+  setupServiceLocator();
   await Hive.openBox<BookEntity>(kFeaturedBox);
   await Hive.openBox<BookEntity>(kNewestBox);
   runApp(const BookApp());
@@ -21,10 +28,23 @@ class BookApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
-      routerConfig: AppRouter.router,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return FeaturedBooksCubit(
+              GetFeaturedBooksUseCase(homeRepo: getIt.get<HomeRepoImp>()),
+            );
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+        ),
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
