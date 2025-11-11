d2b98c08c16a940e4d31dcd62901724e4264b951
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-11-11 15:15:22 +0200

    feat: register adapter and add open featured box

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   // تهيئة Hive للعمل مع Flutter
   await Hive.initFlutter();
+
+  Hive.registerAdapter(BookEntityAdapter());
+
+  await Hive.openBox(kFeaturedBox);
 
   runApp(const BookApp());
 }
