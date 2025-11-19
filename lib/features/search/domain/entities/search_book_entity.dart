24d35d0cd2122757a9af74e9e6248381ea833299
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-11-19 15:47:45 +0200

    feat(search): add Hive support for SearchBook entity

--- lib/features/search/domain/entities/search_book_entity.dart
+++ lib/features/search/domain/entities/search_book_entity.dart
@@ -1,9 +1,19 @@
+import 'package:hive/hive.dart';
+part 'search_book_entity.g.dart'; // لتوليد الكود
+
+@HiveType(typeId: 0)
 class SearchBookEntity {
+  @HiveField(0)
   final String bookID;
+  @HiveField(1)
   final String? bookImage;
+  @HiveField(2)
   final String bookTitle;
+  @HiveField(3)
   final String? bookAuthor;
+  @HiveField(4)
   final num? bookRating;
+  @HiveField(5)
   final String bookPrice;
 
   SearchBookEntity({
---
+++ lib/features/search/domain/entities/search_book_entity.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'search_book_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SearchBookEntityAdapter extends TypeAdapter<SearchBookEntity> {
+  @override
+  final int typeId = 0;
+
+  @override
+  SearchBookEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return SearchBookEntity(
+      bookID: fields[0] as String,
+      bookImage: fields[1] as String?,
+      bookTitle: fields[2] as String,
+      bookAuthor: fields[3] as String?,
+      bookRating: fields[4] as num?,
+      bookPrice: fields[5] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, SearchBookEntity obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.bookID)
+      ..writeByte(1)
+      ..write(obj.bookImage)
+      ..writeByte(2)
+      ..write(obj.bookTitle)
+      ..writeByte(3)
+      ..write(obj.bookAuthor)
+      ..writeByte(4)
+      ..write(obj.bookRating)
+      ..writeByte(5)
+      ..write(obj.bookPrice);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SearchBookEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
